3be6e4097d33c54291441ace3e9322d83748592d
Bram Pijper <brampijper@gmail.com>
2016-10-04 14:35:43 +0200

    Included welcome text in different languages

--- includes/main.js
+++ includes/main.js
@@ -1,3 +1,35 @@
 $ ("document").ready(function () {
-	$('.responsive-img').hide().fadeIn(3000)
+	var words = ["testje","test2","test3","test4"], i = 0; 
+
+
+
+	// $('#imageFade').hide().fadeIn(2000);
+	// $('#welcomeText').hide().delay(2000).fadeIn(1800);
+	
+	
+
+
+
+$('.scrollspy').scrollSpy();
+
 }); 
+
+
+
+
+(function(){
+    var words = [
+        'Velkommen',
+        'Bienvenue',
+        'Welkom',
+        'Wilkommen',
+        'bienvenida',
+        '歡迎'
+        ], i = 0;
+    setInterval(function(){
+        $('#changeWords').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 2000);
+        
+})();
