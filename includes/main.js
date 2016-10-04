8f4510150567a92a80d9ed9d024eb83259abf20a
Bram Pijper <brampijper@gmail.com>
2016-10-04 20:12:56 +0200

    Final Changes -> Moved .js files into includes folder -> Deleted third project box

--- includes/main.js
+++ includes/main.js
@@ -1,31 +1,27 @@
 $ ("document").ready(function () {
 	var words = ["testje","test2","test3","test4"], i = 0; 
-
-
-
-	// $('#imageFade').hide().fadeIn(2000);
-	// $('#welcomeText').hide().delay(2000).fadeIn(1800);
-	
-	
-
-
-
 $('.scrollspy').scrollSpy();
 
-}); 
-
-(function(){
     var words = [
         'Velkommen',
         'Bienvenue',
         'Welkom',
         'Wilkommen',
-        'bienvenida',
-        '歡迎'
+        'Bienvenida',
+        'Welcome'
         ], i = 0;
     setInterval(function(){
         $('#changeWords').fadeOut(function(){
             $(this).html(words[i=(i+1)%words.length]).fadeIn();
         });
     }, 2000);
-})();
+}); 
+
+
+
+
+
+
+
+	// $('#imageFade').hide().fadeIn(2000);
+	// $('#welcomeText').hide().delay(2000).fadeIn(1800);
